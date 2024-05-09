a41f7c739620e7c4d6ea3c8910d92bef7fe0cddb
Simon Liang <simon@x-tech.io>
2024-05-09 21:18:31 +0800

    build and dev with typescript

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,23 @@
-import { createRequestHandler } from "@remix-run/express";
-import { installGlobals } from "@remix-run/node";
-import compression from "compression";
-import express from "express";
+import { createRequestHandler } from '@remix-run/express';
+import { ServerBuild, installGlobals } from '@remix-run/node';
+import compression from 'compression';
+import express from 'express';
 
 installGlobals();
 
 const viteDevServer =
-  process.env.NODE_ENV === "production"
+  process.env.NODE_ENV === 'production'
     ? undefined
-    : await import("vite").then((vite) =>
+    : await import('vite').then((vite) =>
         vite.createServer({
           server: { middlewareMode: true },
-        })
+        }),
       );
 
 const remixHandler = createRequestHandler({
   build: viteDevServer
-    ? () => viteDevServer.ssrLoadModule("virtual:remix/server-build")
-    : await import("../build/server/index.js"),
+    ? () => viteDevServer.ssrLoadModule('virtual:remix/server-build') as Promise<ServerBuild>
+    : await import('../build/server/index.js'),
 });
 
 const app = express();
@@ -25,7 +25,7 @@ const app = express();
 app.use(compression());
 
 // http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
-app.disable("x-powered-by");
+app.disable('x-powered-by');
 
 // handle asset requests
 if (viteDevServer) {
@@ -33,19 +33,19 @@ if (viteDevServer) {
 } else {
   // Vite fingerprints its assets so we can cache forever.
   app.use(
-    "/assets",
-    express.static("build/client/assets", { immutable: true, maxAge: "1y" })
+    '/assets',
+    express.static('build/client/assets', { immutable: true, maxAge: '1y' }),
   );
 }
 
 // Everything else (like favicon.ico) is cached for an hour. You may want to be
 // more aggressive with this caching.
-app.use(express.static("build/client", { maxAge: "1h" }));
+app.use(express.static('build/client', { maxAge: '1h' }));
 
 // handle SSR requests
-app.all("*", remixHandler);
+app.all('*', remixHandler);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () =>
-  console.log(`Express server listening at http://localhost:${port}`)
+  console.log(`Express server listening at http://localhost:${port}`),
 );
